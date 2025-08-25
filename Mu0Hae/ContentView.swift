c18a1abcc49efb5015e7d89402ef4f888bca1994
ParkSeongGeun <phd0801@naver.com>
2025-08-25 18:34:49 +0900

    Project Settings

---
+++ Mu0Hae/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mu0Hae
+//
+//  Created by 박성근 on 8/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mu0Hae/Mu0HaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mu0HaeApp.swift
+//  Mu0Hae
+//
+//  Created by 박성근 on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Mu0HaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

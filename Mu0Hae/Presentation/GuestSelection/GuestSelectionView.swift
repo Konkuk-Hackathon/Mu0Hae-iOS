20df1e6162fa68a77d862548288c10d6582f3730
Guryss <lala1470@konkuk.ac.kr>
2025-08-27 16:17:15 +0900

    design: implement guest selection ui

--- Mu0Hae/Presentation/GuestSelection/GuestSelectionView.swift
+++ Mu0Hae/Presentation/GuestSelection/GuestSelectionView.swift
@@ -8,8 +8,51 @@
 import SwiftUI
 
 struct GuestSelectionView: View {
+    
+    private let itemWidth: CGFloat = (UIApplication.screenWidth - 25 * 3) / 2
+    private var columns: [GridItem] {
+        [
+            GridItem(.fixed(itemWidth)),
+            GridItem(.fixed(itemWidth))
+        ]
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .top) {
+            VStack(spacing: 0) {
+                GuestSelectionNavigationBarView()
+                
+                LazyVGrid(columns: columns, spacing: 25) {
+                    ForEach(1...4, id: \.self) { _ in
+                        GuestSelectionCard()
+                    }
+                }
+                .padding(.top, 25)
+                
+                Spacer()
+            }
+        }
+        .background(Color.muBackground)
+    }
+}
+
+private struct GuestSelectionNavigationBarView: View {
+    var body: some View {
+        ZStack {
+            HStack(spacing: 0) {
+                Image(.icBack)
+                    .onTapGesture {
+                        // navigate Chat View
+                    }
+                Spacer()
+            }
+            .frame(height: 56)
+            .padding(.horizontal, 20)
+            .background(Color.muBackground)
+            .overlay {
+                Image(.imgSettingTItle)
+            }
+        }
     }
 }
 

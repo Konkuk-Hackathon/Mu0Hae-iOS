12f88c83f9540fca191902c12e0b2110cd0cea83
Guryss <lala1470@konkuk.ac.kr>
2025-08-27 16:39:53 +0900

    design: implement GuestDescription Popup

---
+++ Mu0Hae/Presentation/GuestSelection/GuestDescriptionPopup.swift
@@ -0,0 +1,70 @@
+//
+//  GuestDescriptionPopup.swift
+//  Mu0Hae
+//
+//  Created by Seoyeon Choi on 8/27/25.
+//
+
+import SwiftUI
+
+struct GuestDescriptionPopup: View {
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.5)
+            
+            VStack(spacing: 0) {
+                Circle()
+                    .frame(width: 86, height: 86)
+                    .foregroundStyle(.muSecondary)
+                    .padding(.bottom, 15)
+                
+                Text("유병재")
+                    .muFont(.title1)
+                    .foregroundStyle(.muText)
+                    .padding(.bottom, 6)
+                
+                Text("왜 그런지 몰라도, 어떤 사연이든 \n다 공감해드립니다.")
+                    .muFont(.title2)
+                    .foregroundStyle(.muSubText)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 30)
+                
+                HStack(spacing: 20) {
+                    GuestSelectionButton(title: "취소",
+                                         fontColor: .muSubText,
+                                         backgroundColor: .muLightGray)
+                    
+                    GuestSelectionButton(title: "설정하기",
+                                         fontColor: .muBackground,
+                                         backgroundColor: .muPrimary)
+                }
+            }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 30)
+            .background(Color.muBackground)
+            .clipShape(RoundedRectangle(cornerRadius: 14))
+        }
+        .ignoresSafeArea()
+    }
+}
+
+private struct GuestSelectionButton: View {
+    var title: String
+    var fontColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 8)
+            .frame(width: 120, height: 38)
+            .foregroundStyle(backgroundColor)
+            .overlay {
+                Text(title)
+                    .muFont(.buttonTitle)
+                    .foregroundStyle(fontColor)
+            }
+    }
+}
+
+#Preview {
+    GuestDescriptionPopup()
+}
